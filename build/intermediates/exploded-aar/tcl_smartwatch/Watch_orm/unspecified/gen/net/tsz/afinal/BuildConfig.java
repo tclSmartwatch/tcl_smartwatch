37e2a96f336efc405af160775b29caa00c623adb
lillian815 <woyaofeixiangyan@163.com>
2014-12-26 11:45:43 +0800

    update

---
+++ build/intermediates/exploded-aar/tcl_smartwatch/Watch_orm/unspecified/gen/net/tsz/afinal/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.tsz.afinal;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ build/intermediates/manifests/full/debug/gen/com/tcl/watch/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tcl.watch;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
